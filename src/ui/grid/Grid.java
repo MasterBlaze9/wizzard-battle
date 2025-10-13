e8e3e4cf990f2be617c79e58b1295e6ce3ca7fc2
Rui Simões <ruinunoss@gmail.com>
2025-10-13 12:16:25 +0100

    feat: Add AppColor utility for color management and update Grid initialization

--- src/ui/grid/Grid.java
+++ src/ui/grid/Grid.java
@@ -2,9 +2,11 @@ package ui.grid;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
-public class Grid  {
+import utils.AppColor;
 
-	public static final int PADDING = 10;
+public class Grid {
+
+    public static final int PADDING = 10;
 
     private int cellSize = 15;
     private int cols;
@@ -18,7 +20,7 @@ public class Grid  {
      * @param cols number of the columns
      * @param rows number of rows
      */
-    public Grid(int cols, int rows){
+    public Grid(int cols, int rows) {
         this.cols = cols;
         this.rows = rows;
     }
@@ -28,20 +30,19 @@ public class Grid  {
      */
 
     public void init() {
-        this.field = new Rectangle(PADDING,PADDING, cols * cellSize, rows * cellSize);
-        this.field.draw();
+        this.field = new Rectangle(PADDING, PADDING, cols * cellSize, rows * cellSize);
+        this.field.setColor(AppColor.BROWN.toColor());
+        this.field.fill();
     }
 
     public int getCellSize() {
         return cellSize;
     }
 
-
     public int getCols() {
         return this.cols;
     }
 
-   
     public int getRows() {
         return this.rows;
     }
@@ -61,7 +62,5 @@ public class Grid  {
     public int getY() {
         return this.field.getY();
     }
-
-   
 
 }
