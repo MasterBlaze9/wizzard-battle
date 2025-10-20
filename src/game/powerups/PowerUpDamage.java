a90d436cee2dd6e968d56dda9acd6153d7f74765
Rui Simões <72996460+MasterBlaze9@users.noreply.github.com>
2025-10-20 14:19:29 +0100

    Delete src/game/powerups directory

--- src/game/powerups/PowerUpDamage.java
+++
@@ -1,9 +0,0 @@
-package game.powerUps;
-
-
-public class PowerUpDamage extends PowerUp {
-
-    public PowerUpDamage(int col, int row) {
-        super(col, row,"resources/PowerUps/damage.png");
-    }
-}
--- src/game/powerups/PowerupType.java
+++
@@ -1,5 +0,0 @@
-package game.powerups;
-
-public enum PowerupType {
-
-}
